28793da784b2f1b8c227ce35ffc2f44738efbf14
simca09 <delacruz.klydean@yahoo.com>
2022-12-10 21:10:42 -0500

    Changed class of period in html, linked js to html

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const screen = document.querySelector(".screen");
+const operators = document.querySelectorAll(".operators");
+const numbers = document.querySelectorAll(".numbers");
+const allClear = document.querySelector("#allClear");
+const clear = document.querySelector("#clear");
+const subtraction = document.querySelector("#subtraction");
+const division = document.querySelector("#division");
+const multiplication = document.querySelector("#multiplication");
+const addition = document.querySelector("#addition");
+const equals = document.querySelector("#equals");
+const period = document.querySelector("#period");
+const nine = document.querySelector("#buttonNine");
+const eight = document.querySelector("#buttonEight");
+const seven = document.querySelector("#buttonSeven");
+const six = document.querySelector("#buttonSix");
+const five = document.querySelector("#buttonFive");
+const four = document.querySelector("#buttonFour");
+const three = document.querySelector("#buttonThree");
+const two = document.querySelector("#buttonTwo");
+const one = document.querySelector("#buttonOne");
+const zero = document.querySelector("#buttonZero");
+
+
