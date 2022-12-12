971202f8762428fd298f48a7d81b24f01e84c0f9
simca09 <delacruz.klydean@yahoo.com>
2022-12-11 20:34:15 -0500

    Number and operator functions still in progpress

--- javascript.js
+++ javascript.js
@@ -1,4 +1,16 @@
-const screen = document.querySelector(".screen");
+console.log("Sheesh")
+
+let startValue  = "";
+let secondValue = "";
+let operator = "";
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+// Store all components of HTML in JS
+const screen = document.querySelector("#screen");
+const screenOne = document.querySelector(".screenOne");
+const screenTwo = document.querySelector(".screenTwo");
 const operators = document.querySelectorAll(".operators");
 const numbers = document.querySelectorAll(".numbers");
 const allClear = document.querySelector("#allClear");
@@ -19,5 +31,52 @@ const three = document.querySelector("#buttonThree");
 const two = document.querySelector("#buttonTwo");
 const one = document.querySelector("#buttonOne");
 const zero = document.querySelector("#buttonZero");
+
+
+
+// Number functions 1/2
+numbers.forEach((numb) => numb.addEventListener("click", function(e){
+        handleNumber(e.target.textContent);
+        screenOne.textContent = startValue;
+    }))
+
+// Operator functions 1/2
+operators.forEach((ope) => ope.addEventListener("click", function(e){
+        handleOperator(e.target.textContent);
+        screenOne.textContent = startValue + opa;
+        screenTwo.textContent = secondValue;
+    }))
+
+})
+
+// Number functions 2/2
+function handleNumber(num) {
+    if (startValue.length <= 5) {
+        startValue += num;
+    }
+}
+
+
+// Operator functions 2/2
+
+function handleOperator(op){
+    opa = op;
+    secondValue = startValue;
+    startValue = "";
+
+}
+
+
+
+
+
+
+
+
+// Calculator options
+// const add = (num1, num2) => num1 + num2;
+// const minus = (num1, num2) => num1 - num2;
+// const multiply = (num1, num2) => num1 * num2;
+// const divide = (num1, num2) => num1 / num2;
 
 
